5a0dabc1301b5b53b171f4b1b679d3adc56f2df0
Svetlana Trukhina <sweta4ca@mail.ru>
2023-10-21 20:40:41 +0700

    Updates

--- spa/assets/index.623a918e.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.2c6b3d16.js";import{d as a,o as t,c as o,w as n,a as s}from"./index.29b76fb7.js";const r=s("h1",null,"Catalog page",-1),c=a({__name:"index",setup(l){return a({name:"CatalogPage"}),(_,p)=>(t(),o(e,null,{default:n(()=>[r]),_:1}))}});var g=c;export{g as default};
--- spa/assets/index.907191e4.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./QPage.2c6b3d16.js";import{d as e,o,c as t,w as r,a as n}from"./index.29b76fb7.js";const s=n("h1",null,"Profile page",-1),c=e({__name:"index",setup(l){return e({name:"ProfilePage"}),(_,i)=>(o(),t(a,null,{default:r(()=>[s]),_:1}))}});var m=c;export{m as default};
